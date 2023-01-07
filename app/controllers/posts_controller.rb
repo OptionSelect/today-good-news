aeca7b0802890ba4be92d9a44839f2fee9be7e90
Brandyn Sullins <brandyn.sullins@gmail.com>
2023-01-07 18:37:55 -0500

    Now able to get reddit titles and urls and analyze sentiment score

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,27 @@
 require 'rest-client'
+require "textmood"
 
 class PostsController < ApplicationController
 
   def get_posts
-    url = "https://www.reddit.com/r/learnprogramming"+".json"
+    tm = TextMood.new(language: "en")
+    minimum_score = 0.05
+
+    url = "https://www.reddit.com/r/news"+".json"
     response = RestClient.get(url)
     posts = JSON.parse(response.body)
-    puts posts
-    render json: posts
+
+    for post in posts["data"]["children"]
+
+      sentiment_score = tm.analyze(post["data"]["title"])
+
+      if post["data"]["url"].present? && sentiment_score > minimum_score
+        title = post["data"]["title"]
+        url = post["data"]["url"]
+        puts "#{title} - #{url} - Score:#{score}"
+      end      
+    end
+
+
   end
 end
